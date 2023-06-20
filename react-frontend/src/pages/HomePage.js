1cc2fdf725400eb69420f87ee533e0fe4d9f4343
UTKARSH SHAH <ut331358@dal.ca>
2023-06-19 21:33:55 -0300

    Added: Router Structure and Basic Skeleton of react app

---
+++ react-frontend/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function HomePage(){
+    return(
+
+        
+            <div>
+                <h1>
+                    Home Page
+                </h1>
+                <p>
+                    welcome to the home page
+                </p>
+            </div>
+       
+    )
+}
