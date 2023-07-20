4e6c76677b670eb6ce5c9731d7dba4f307356368
Sakib Sadman <ssadman3659@gmail.com>
2023-07-20 17:11:17 -0300

    Update Backend Product Schema

--- MongoDBServer/models/product.js
+++ MongoDBServer/models/product.js
@@ -1,14 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const product = new mongoose.Schema({
-   
-    productID:{
-        type:String,
-        required:true
-    },
-    productName:{
-        type:String,
-        required:true
-    }
-})
-module.exports = mongoose.model('Product',product)
+  productID: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    alias: "productName",
+  },
+  fileUpload: {
+    type: String, // TODO: change later
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  province: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", product);
