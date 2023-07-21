42428861cdbf569dea4c506bac76405daaa4b2ad
Sakib Sadman <ssadman3659@gmail.com>
2023-07-20 21:03:01 -0300

    Add listing button in navbar

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
 
   const userToggleDrop = () => {
     setIsToggleOpen((isToggleOpen) => !isToggleOpen);
-  }
+  };
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -56,7 +56,9 @@ export default function Navbar() {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <Link to="/" className="flex items-center">
-                <h1 className="text-white font-bold text-xl italic" >Swap Sphere</h1>
+                <h1 className="text-white font-bold text-xl italic">
+                  Swap Sphere
+                </h1>
               </Link>
             </div>
             <div className="hidden sm:block sm:ml-6">
@@ -98,7 +100,6 @@ export default function Navbar() {
 
             <div className="flex-1 flex sm:justify-end">
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
                 <div className="relative ml-3">
                   {/* Search bar */}
                   <div className="relative flex items-center w-full h-8 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
@@ -129,36 +130,89 @@ export default function Navbar() {
                 </div>
                 {/* Wishlist section */}
                 <div className="relative ml-3">
-                  <Link to="/wishlist" className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <Link
+                    to="/wishlist"
+                    className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  >
                     <span className="sr-only">Open wishlist</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                      />
                     </svg>
                     {/* <span className="ml-1 text-white">{wishlistCount}</span> */}
                   </Link>
                 </div>
                 {/* <!-- Profile dropdown --> */}
                 <div className="relative ml-3">
-
                   <div>
-                    <button type="button" className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded={isToggleOpen} aria-haspopup="true" onClick={userToggleDrop}>
+                    <button
+                      type="button"
+                      className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      id="user-menu-button"
+                      aria-expanded={isToggleOpen}
+                      aria-haspopup="true"
+                      onClick={userToggleDrop}
+                    >
                       <span className="sr-only">Open user menu</span>
-                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
                     </button>
                   </div>
 
-
                   {isToggleOpen && (
-                    <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                    <div
+                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="user-menu-button"
+                      tabIndex="-1"
+                    >
                       {/* Dropdown menu items */}
-                      <a href="/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Login</a>
-                      <a href="/settings" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
+                      <a
+                        href="/profile"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="user-menu-item-0"
+                      >
+                        Login
+                      </a>
+                      <a
+                        href="/settings"
+                        className="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        tabIndex="-1"
+                        id="user-menu-item-1"
+                      >
+                        Settings
+                      </a>
                     </div>
                   )}
                 </div>
 
+                {/* New listing section */}
+                <div className="relative ml-3">
+                  <Link
+                    to="/createListing"
+                    className=" flex bg-blue-300 hover:bg-blue-500 text-black font-bold py-2 px-4 rounded"
+                  >
+                    New Listing
+                  </Link>
+                </div>
               </div>
-
             </div>
           </div>
         </div>
