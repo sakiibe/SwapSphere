92b6e9024fc0d5322f58096b17c81ca0e8f586d1
UTKARSH SHAH <ut331358@dal.ca>
2023-07-26 21:15:36 -0300

    Landing Page added

--- react-frontend/src/pages/HomePage.js
+++ react-frontend/src/pages/HomePage.js
@@ -33,71 +33,9 @@ export default function HomePage() {
       });
   }, []);
 
-  // categories and subcategories feature
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
-
-
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-    setSelectedSubcategory(null);
-  };
-
-  const handleSubcategoryChange = (subcategory) => {
-    setSelectedSubcategory(subcategory);
-  };
-  const categories = [
-    {
-      id: 1,
-      name: "Home Appliance",
-      subcategories: ["Kitchen", "Living Room", "Bedroom", "Bathroom"],
-    },
-    {
-      id: 2,
-      name: "Electronics",
-      subcategories: [
-        "Laptop",
-        "Phones",
-        "Computer Accessories",
-        "Video Games & Consoles",
-        "Smart Watch",
-      ],
-    },
-    {
-      id: 3,
-      name: "Computer Parts",
-      subcategories: ["Hard-disk", "Screen", "Keyboards"],
-    },
-    {
-      id: 4,
-      name: "Vehile",
-      subcategories: ["Car", "Motorcycle", "Bicycle", "Truck"],
-    },
-    {
-      id: 5,
-      name: "Furniture",
-      subcategories: ["Table", "Chair", "Cabinet", "Bed"],
-    },
-    {
-      id: 6,
-      name: "Instruments",
-      subcategories: ["Guitar", "Piano", "Violin", "Drums"],
-    },
-    {
-      id: 7,
-      name: "Tools",
-      subcategories: ["Hand Tools", "Power Tools", "Gardening Tools", "Automotive Tools"]
-    }
-
-  ];
 
 
 
-  const filteredProductListings = products.filter(
-    (product) =>
-      product.category === selectedCategory &&
-      (selectedSubcategory === null || product.subcategory === selectedSubcategory)
-  );
 
   // wishlist feature
   const [wishlist, setWishlist] = useState([]);
@@ -152,111 +90,33 @@ export default function HomePage() {
     <div>
       <Navbar />
       <div className="flex sm:py-12">
-        <div className="w-1/5 px-4 py-8">
-          {/* Sidebar */}
-          <h2 className="text-2xl font-bold mb-4">Categories</h2>
-          <ul className="space-y-2">
-            {categories.map((category) => (
-              <li key={category.id}>
-                <button
-                  className={`${category.name === selectedCategory
-                    ? "bg-gray-200"
-                    : "bg-gray-100"
-                    } w-full px-4 py-2 rounded-md`}
-                  onClick={() => handleCategoryChange(category.name)}
-                >
-                  {category.name}
-                </button>
-                {selectedCategory === category.name && (
-                  <ul className="pl-5 space-y-2">
-                    {category.subcategories.map((subcategory) => (
-                      <li key={subcategory}>
-                        <button
-                          className={`${subcategory === selectedSubcategory
-                            ? "bg-gray-200"
-                            : "bg-gray-100"
-                            } w-full px-6 py-2 rounded-md`}
-                          onClick={() => handleSubcategoryChange(subcategory)}
-                        >
-                          {subcategory}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="w-4/5 px-4 py-8">
-          {/* Product Listing */}
-          {selectedCategory && selectedSubcategory ? (
-            <h2 className="text-2xl font-bold mb-4">
-              Products in {selectedSubcategory}
-            </h2>
-          ) : (
-            <h2 className="text-2xl font-bold mb-4">All Products</h2>
-          )}
+        <div className="w-full px-4 py-8">
+          <h2 className="text-2xl font-bold mb-4">All Products</h2>
           <div className="grid grid-cols-3 gap-4">
-            {selectedCategory || selectedSubcategory ? (
-              filteredProductListings.length > 0 ? (
-                filteredProductListings.map((product) => (
-                  <div key={product.id}>
-                    <Card
-                      title={product.title}
-                      imageSrc={product.imageSrc}
-                      price={product.price}
-                      email={product.email}
-                      productID={product.productID}
-                    />
-
-                    {/* insert slider 1 image here */}
-                    < img src={slider1} alt="Slider 1" />
-
-                    {isWishlisted(product.id) ? (
-                      <button disabled className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                        </svg>
-                      </button>
-                    ) : (
-                      <button onClick={() => handleWishlistButtonClick(product)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-                        </svg>
-                      </button>
-                    )}
-                  </div>
-                ))
-              ) : (
-                <p>No products found.</p>
-              )
-            ) : (
-              products.map((product) => (
-                <div key={product.productID}>
-                  <Card
-                    title={product.title}
-                    imageSrc={product.imageSrc}
-                    price={product.price}
-                    email={product.email}
-                    productID={product.productID}
-                  />
-                  {isWishlisted(product.id) ? (
-                    <button disabled className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </button>
-                  ) : (
-                    <button onClick={() => handleWishlistButtonClick(product)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-                      </svg>
-                    </button>
-                  )}
-                </div>
-              ))
-            )}
+            {products.map((product) => (
+              <div key={product.id}>
+                <Card
+                  title={product.title}
+                  imageSrc={product.imageSrc}
+                  price={product.price}
+                  email={product.email}
+                  productID={product.productID}
+                />
+                {isWishlisted(product.id) ? (
+                  <button disabled className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </button>
+                ) : (
+                  <button onClick={() => handleWishlistButtonClick(product)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                    </svg>
+                  </button>
+                )}
+              </div>
+            ))}
           </div>
           <ToastContainer position="bottom-right" />
         </div>
