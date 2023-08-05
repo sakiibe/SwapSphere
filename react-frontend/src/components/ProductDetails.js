850a32bf946fb1bb3200d24cdb5abee6eb8950d3
tapanpatel <tapan.patel@dal.ca>
2023-08-05 02:18:33 -0300

    made ui changes to comment feature

--- react-frontend/src/components/ProductDetails.js
+++ react-frontend/src/components/ProductDetails.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import useWishlist from "../pages/useWishlist";
 import { ToastContainer, toast } from 'react-toastify';
-import { Button, TextField } from '@mui/material';
-import { Favorite, AddShoppingCart } from '@mui/icons-material';
+import { Favorite, AddShoppingCart, Edit, Delete, Send, Save } from '@mui/icons-material';
+import { IconButton, TextField, Avatar, Typography, Box, Divider } from '@mui/material';
 
 const ProductDetails = () => {
   const [productDetails, setProductDetails] = useState({});
@@ -15,39 +15,31 @@ const ProductDetails = () => {
   const loggedInUser = localStorage.getItem('email');
 
   useEffect(() => {
-    // Call the API to get the product details
     fetch(`http://localhost:8080/product/product/${productID}`)
       .then((response) => response.json())
       .then((data) => {
         if (data.error) {
-          // Show error toast notification
           toast.error('An error occurred while fetching the product.');
         } else {
-          // Set the fetched product details to state
           setProductDetails(data);
         }
       })
       .catch((error) => {
-        // Show error toast notification
         toast.error('An error occurred while fetching the product.');
         console.log(error);
       });
 
-    // Call the API to get all comments for the productID in ascending order of createdAt
     fetch(`http://localhost:8080/comment/getAll/${productID}`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          // Set the fetched comments to state (sorted in ascending order of createdAt)
           const sortedComments = data.comments.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
           setComments(sortedComments);
         } else {
-          // Show error toast notification
           toast.error('An error occurred while fetching the comments.');
         }
       })
       .catch((error) => {
-        // Show error toast notification
         toast.error('An error occurred while fetching the comments.');
         console.log(error);
       });
@@ -58,7 +50,6 @@ const ProductDetails = () => {
   };
 
   const handleCommentSubmit = () => {
-    // Assuming you have a server endpoint to handle comment submission
     fetch('http://localhost:8080/comment/add', {
       method: 'POST',
       headers: {
@@ -73,40 +64,121 @@ const ProductDetails = () => {
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          // Show success toast notification
           toast.success('Comment submitted successfully.');
-          // Clear the comment text field
           setCommentText('');
-          // Fetch the updated comments list after successful submission
           fetch(`http://localhost:8080/comment/getAll/${productID}`)
             .then((response) => response.json())
             .then((data) => {
               if (data.success) {
-                // Set the fetched comments to state (sorted in ascending order of createdAt)
                 const sortedComments = data.comments.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
                 setComments(sortedComments);
               } else {
-                // Show error toast notification
                 toast.error('An error occurred while fetching the comments.');
               }
             })
             .catch((error) => {
-              // Show error toast notification
               toast.error('An error occurred while fetching the comments.');
               console.log(error);
             });
         } else {
-          // Show error toast notification
           toast.error('An error occurred while submitting the comment.');
         }
       })
       .catch((error) => {
-        // Show error toast notification
         toast.error('An error occurred while submitting the comment.');
         console.log(error);
       });
   };
-  
+
+  // Function to handle edit comment
+  const handleEditComment = (commentId, newCommentText) => {
+    fetch(`http://localhost:8080/comment/update/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        commentText: newCommentText,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          toast.success('Comment updated successfully.');
+          fetch(`http://localhost:8080/comment/getAll/${productID}`)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.success) {
+                const sortedComments = data.comments.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+                setComments(sortedComments);
+              } else {
+                toast.error('An error occurred while fetching the comments.');
+              }
+            })
+            .catch((error) => {
+              toast.error('An error occurred while fetching the comments.');
+              console.log(error);
+            });
+        } else {
+          toast.error('An error occurred while updating the comment.');
+        }
+      })
+      .catch((error) => {
+        toast.error('An error occurred while updating the comment.');
+        console.log(error);
+      });
+  };
+
+  // Function to handle delete comment
+  const handleDeleteComment = (commentId) => {
+    fetch(`http://localhost:8080/comment/delete/${commentId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          toast.success('Comment deleted successfully.');
+          fetch(`http://localhost:8080/comment/getAll/${productID}`)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.success) {
+                const sortedComments = data.comments.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+                setComments(sortedComments);
+              } else {
+                toast.error('An error occurred while fetching the comments.');
+              }
+            })
+            .catch((error) => {
+              toast.error('An error occurred while fetching the comments.');
+              console.log(error);
+            });
+        } else {
+          toast.error('An error occurred while deleting the comment.');
+        }
+      })
+      .catch((error) => {
+        toast.error('An error occurred while deleting the comment.');
+        console.log(error);
+      });
+  };
+
+  // New state to track the edited comment and its id
+  const [editedCommentId, setEditedCommentId] = useState('');
+  const [editedCommentText, setEditedCommentText] = useState('');
+
+  // Function to toggle the edit mode for a comment
+  const toggleEditMode = (commentId, commentText) => {
+    if (commentId === editedCommentId) {
+      // Save the edited comment when the user clicks on the icon again
+      handleEditComment(commentId, editedCommentText);
+      setEditedCommentId('');
+      setEditedCommentText('');
+    } else {
+      // Set the comment text to be edited when the user clicks on the "Edit" icon
+      setEditedCommentId(commentId);
+      setEditedCommentText(commentText);
+    }
+  };
 
   return (
     <div className="p-4">
@@ -115,28 +187,79 @@ const ProductDetails = () => {
       <p className="text-gray-800 font-bold">${productDetails.price}</p>
       <p className="text-gray-500">Category: {productDetails.category}</p>
       <div className="mt-4">
-        <Button
+        <IconButton
           onClick={() => addToWishlist(productDetails)}
-          variant="contained"
-          startIcon={<AddShoppingCart />}
           color="primary"
           disabled={wishlistLoading}
         >
-          {wishlistLoading ? 'Adding...' : 'Add to Wishlist'}
-        </Button>
-        <button className="mr-2 px-4 py-2 bg-gray-500 text-white rounded">Share</button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded">Report Item</button>
+          <AddShoppingCart />
+        </IconButton>
+        <IconButton>
+          <Favorite />
+        </IconButton>
+        <IconButton>
+          <Edit />
+        </IconButton>
+        <IconButton>
+          <Delete />
+        </IconButton>
       </div>
-      <div className="mt-4">
-        {/* Display comments */}
+      <Divider className="my-4" />
+
+      <Box sx={{ mt: 4 }}>
         {comments.map((comment) => (
-          <div key={comment._id} className="mb-2">
-            <p className="text-gray-600">{comment.commentText}</p>
-            <p className="text-gray-500">Posted by: {comment.useremail}</p>
-            <p className="text-gray-500">Date: {new Date(comment.createdAt).toLocaleString()}</p>
-          </div>
+          <Box key={comment._id} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', mb: 2 }}>
+            <Avatar sx={{ bgcolor: 'primary.main', mr: 2 }}>
+              <Favorite />
+            </Avatar>
+            {editedCommentId === comment._id ? (
+              // Show an editable text field when the comment is in edit mode
+              <TextField
+                multiline
+                fullWidth
+                value={editedCommentText}
+                onChange={(event) => setEditedCommentText(event.target.value)}
+              />
+            ) : (
+              // Show the comment text when not in edit mode
+              <Typography variant="body1" sx={{ color: 'text.secondary', mb: 1 }}>
+                {comment.commentText}
+              </Typography>
+            )}
+            <Typography variant="caption" sx={{ color: 'text.disabled' }}>
+              Posted by: {comment.useremail}, Date: {new Date(comment.createdAt).toLocaleString()}
+            </Typography>
+            {comment.useremail === loggedInUser && (
+              <div>
+                {editedCommentId === comment._id ? (
+                  // Show save button when editing the comment
+                  <IconButton color="primary" onClick={() => toggleEditMode(comment._id, comment.commentText)}>
+                    <Save />
+                  </IconButton>
+                ) : (
+                  // Show edit button when not in edit mode
+                  <IconButton color="black" onClick={() => toggleEditMode(comment._id, comment.commentText)}>
+                    <Edit />
+                  </IconButton>
+                )}
+                {!editedCommentId && ( // Hide delete button when editing the comment
+                  <IconButton color="error"
+                    onClick={() => {
+                      if (window.confirm('Are you sure you want to delete this comment?')) {
+                        handleDeleteComment(comment._id);
+                      }
+                    }}
+                  >
+                    <Delete />
+                  </IconButton>
+                )}
+              </div>
+            )}
+            <Divider sx={{ mt: 1, mb: 2 }} />
+          </Box>
         ))}
-      </div>
+      </Box>
+
       <TextField
         label="Write a comment"
         variant="outlined"
@@ -145,9 +268,9 @@ const ProductDetails = () => {
         value={commentText}
         onChange={handleCommentChange}
       />
-      <Button onClick={handleCommentSubmit} variant="contained" color="primary">
-        Submit Comment
-      </Button>
+      <IconButton color="primary" onClick={handleCommentSubmit}>
+        <Send />
+      </IconButton>
       <ToastContainer position="bottom-right" />
     </div>
   );
