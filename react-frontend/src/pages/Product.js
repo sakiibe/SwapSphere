74c5c8dc09801c2d9a01f83bd01ec4557c84d9c2
Sakib Sadman <ssadman3659@gmail.com>
2023-08-06 04:52:28 -0300

    Add socials logos

--- react-frontend/src/pages/Product.js
+++ react-frontend/src/pages/Product.js
@@ -3,21 +3,23 @@ import ImagePanel from "../components/ImagePanel";
 import ProductDetails from "../components/ProductDetails";
 import UserDetails from "../components/UserDetails";
 import Navbar from "../components/Navbar";
+import Share from "../components/Share";
 import Footer from "../components/Footer";
 
 export default function Product() {
-  return (  
-  <div>
-    <Navbar />
-    <div className="flex flex-wrap">
-      <div className="w-1/2 p-4">
-        <ImagePanel />
+  return (
+    <div>
+      <Navbar />
+      <div className="flex flex-wrap">
+        <div className="w-1/2 p-4">
+          <ImagePanel />
+        </div>
+        <div className="w-1/2 p-4">
+          <ProductDetails />
+          <Share />
+          <UserDetails />
+        </div>
       </div>
-      <div className="w-1/2 p-4">
-        <ProductDetails />
-        <UserDetails />
-      </div>
-    </div>
     </div>
   );
 }
