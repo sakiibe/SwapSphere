f087467fbcdbdf8e43a6bb8c53226aaa775703bf
UTKARSH SHAH <ut331358@dal.ca>
2023-08-06 22:32:24 -0300

    Search Functionality Added

--- react-frontend/src/pages/HomePage.js
+++ react-frontend/src/pages/HomePage.js
@@ -18,6 +18,7 @@ export default function HomePage() {
   const { addToWishlist, wishlistLoading } = useWishlist();
 
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -29,7 +30,7 @@ export default function HomePage() {
         console.log("response prods", response.data.products);
         console.log("data is ")
         console.log(products)
-     
+
       })
       .catch((error) => {
         console.error(error);
@@ -37,17 +38,32 @@ export default function HomePage() {
   }, []);
 
 
- 
-  
+  const filteredProducts = products.filter(product =>
+    product.productName.toLowerCase().includes(search.toLowerCase())
+  );
+
 
   return (
     <div>
       <Navbar />
       <div className="flex sm:py-12">
         <div className="w-full px-4 py-8">
-          <h2 className="text-2xl font-bold mb-4">All Products</h2>
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-bold">
+              {search ? `Search results for "${search}"` : "All Products"}
+            </h2>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search products..."
+              className="p-2 border rounded-md"
+              style={{ width: '250px' }}  // Adjust width based on your preference
+            />
+          </div>
           <div className="grid grid-cols-3 gap-4">
-            {products.map((product) => (
+            {filteredProducts.map((product) => (
+              // ... (rest of the code remains unchanged)
               <div key={product.id}>
                 <Card
                   title={product.productName}
@@ -58,13 +74,13 @@ export default function HomePage() {
                   location={product.city}
                 />
                 <Button
-              onClick={() => addToWishlist(product)}
-              variant="contained"
-              startIcon={<AddShoppingCart />}
-              color="primary"
-            >
-            </Button>
-                
+                  onClick={() => addToWishlist(product)}
+                  variant="contained"
+                  startIcon={<AddShoppingCart />}
+                  color="primary"
+                >
+                </Button>
+
               </div>
             ))}
           </div>
