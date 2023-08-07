027e9ec9392ec4e03b15facd9744dfcf4c618ac5
Sakib Sadman <ssadman3659@gmail.com>
2023-08-07 17:52:58 -0300

    Change css of socials

--- react-frontend/src/components/Share.js
+++ react-frontend/src/components/Share.js
@@ -3,41 +3,45 @@ import React from "react";
 function Share() {
   const url = window.location.href; // Get page URL
   const text = "Check out this amazing product!";
+  const encodedUrl = encodeURIComponent(url);
+  const encodedText = encodeURIComponent(text);
 
   return (
     <div className="mt-4 flex items-center">
-      <span className="mr-4">Share:</span>
+      {/* <span className="mr-4 text-base text-black font-medium border-b">
+        Share
+      </span> */}
 
       {/* Facebook Share Button */}
       <a
-        href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
+        href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
         target="_blank"
         rel="noopener noreferrer"
         className="mr-4"
       >
         <img
           src={
-            require("../images/Share-buttons/iconmonstr-facebook-6.svg").default
+            require("../images/Share-buttons/iconmonstr-facebook-5.svg").default
           }
           alt="Facebook"
-          className="w-5 h-5"
+          className="w-10 h-10"
         />
       </a>
 
       {/* Messenger Share Button */}
       <a
-        href={`https://www.facebook.com/dialog/send?link=${url}&app_id=1601543886921602`}
+        href={`https://www.facebook.com/dialog/send?link=${encodedUrl}&app_id=1601543886921602&redirect_uri=${encodedUrl}`}
         target="_blank"
         rel="noopener noreferrer"
         className="mr-4"
       >
         <img
           src={
-            require("../images/Share-buttons/iconmonstr-facebook-messenger-4.svg")
+            require("../images/Share-buttons/iconmonstr-facebook-messenger-5.svg")
               .default
           }
           alt="Messenger"
-          className="w-5 h-5"
+          className="w-10 h-10"
         />
       </a>
 
@@ -50,10 +54,10 @@ function Share() {
       >
         <img
           src={
-            require("../images/Share-buttons/iconmonstr-twitter-4.svg").default
+            require("../images/Share-buttons/iconmonstr-twitter-5.svg").default
           }
           alt="Twitter"
-          className="w-5 h-5"
+          className="w-10 h-10"
         />
       </a>
 
@@ -66,11 +70,11 @@ function Share() {
       >
         <img
           src={
-            require("../images/Share-buttons/iconmonstr-pinterest-4.svg")
+            require("../images/Share-buttons/iconmonstr-pinterest-5.svg")
               .default
           }
           alt="Pinterest"
-          className="w-5 h-5"
+          className="w-10 h-10"
         />
       </a>
 
@@ -84,7 +88,7 @@ function Share() {
             require("../images/Share-buttons/iconmonstr-gmail-5.svg").default
           }
           alt="Email"
-          className="w-5 h-5"
+          className="w-10 h-10"
         />
       </a>
     </div>
